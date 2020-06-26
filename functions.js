2bdfe8e79265f302f040cdd46d4b95f261461534
Roy Matthew Ramos <roymr@uci.edu>
2020-06-25 20:36:12 -0700

    Some updated visuals but still needs work

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+// global variables
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = null;
+let computerSelection = null;
+
 function computerPlay()
 {
     return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
@@ -10,31 +16,66 @@ function playRound(playerSelection, computerSelection)
 
     if(player > comp)
     {
-        return "You win!"
+        playerScore++;
+        return `You win! ${player} beats ${comp}`;
     }
     else if(comp > player) 
     {
-        return "You lose!"
+        computerScore++;
+        return `You lose! ${comp} beats ${player}`;
+        
     }
     else 
     {
-        return "Tie Game!"
+        return "Tie Game!";
     }
 }
 
-function game() 
-{
-    while(true){
-        let playerSelection = prompt("Enter one of the following:\n1 for rock\n2 for paper\n3 for scissors");
+// function game() 
+// {
+//     while(true){
+//         let playerSelection = prompt("Enter one of the following:\n1 for rock\n2 for paper\n3 for scissors");
 
-        if(playerSelection === null){ break; }
+//         if(playerSelection === null){ break; }
 
-        let computerSelection = computerPlay();
+//         let computerSelection = computerPlay();
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+
+//     console.log("Game Over!");
+// }
+
+function game(){
+    // buttons is a node list. It looks and acts much like an array.
+    const buttons = document.querySelectorAll('button');
+    
+    // we use the .forEach method to iterate through each button
+    buttons.forEach((button) => {
+        // and for each one we add a 'click' listener
+        button.addEventListener('click', (e) => {
+        
+        console.log('----------------------------------------------')
+        if(computerScore == 5 || playerScore == 5)
+        {
+            console.log('We have a winner');
+            let x = (playerScore == 5) ? "Player wins!" : "Computer Wins :(";
+            console.log(x);
+            playerScore = 0; 
+            computerScore = 0;
+            return;
+        }
+
+
+        computerSelection = computerPlay();
+        playerSelection = button.id;
+        // alert(button.id);
+        // console.log(button.id)
         console.log(playRound(playerSelection, computerSelection));
-    }
-
-    console.log("Game Over!");
+        console.log(`Score is now:\nPlayer: ${playerScore} AI: ${computerScore}`);
+        });
+    });
 }
+
 
 game();
 
